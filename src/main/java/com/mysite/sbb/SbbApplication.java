77342ba5da341cf3822a4a13b181322d0945e91c
kariyarn <kariyarn@hanmail.net>
2023-10-10 17:48:45 +0900

    3-05까지 완료 내일 마무리한다.

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -1,10 +1,16 @@
 package com.mysite.sbb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mysite.sbb.question.QuestionService;
+
 @SpringBootApplication
 public class SbbApplication {
+	
+	@Autowired
+	private QuestionService questionService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbbApplication.class, args);
