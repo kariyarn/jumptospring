b7291f6b970b7f387264877a897373f4b1aa31ed
kariyarn <kariyarn@hanmail.net>
2023-10-11 21:50:59 +0900

    3-6까지 완료

---
+++ src/main/java/com/mysite/sbb/user/SiteUser.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)//유일한 값 할당
+	private String username;
+	
+	private String password;
+	
+	@Column(unique = true)
+	private String email;
+}
